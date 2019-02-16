27a9ea0b98d695b3cde44208a61a1e6f195a676e
COYOSSS <yossefcohzar@gmail.com>
2019-02-16 22:35:36 +0200

    creation class Bill - a customer and data what he buy

--- src/com/company/Bill.java
+++ src/com/company/Bill.java
@@ -1,5 +1,21 @@
 package com.company;
 
-public class Bill {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Bill {
+    private Customer customer;
+    private Map<Product, Integer> products = new HashMap<Product,Integer>();
+
+    public void addProduct (Product produit,Integer nombre) {
+    products.put(produit, nombre);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
 }
