d35bd28e5c0e127629a21831ba8cae4e42d06b43
COYOSSS <yossefcohzar@gmail.com>
2019-02-16 22:40:55 +0200

    add constructor at Customer

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -6,6 +6,10 @@ public class Customer {
    private String fullName;
    private String adress;
 
+   public  Customer (String fullName,String adress){
+       this.fullName = fullName;
+       this.adress = adress;
+   }
     public String getFullName() {
         return fullName;
     }
